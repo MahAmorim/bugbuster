c0a59dd6812963d09a89278a599e3cb1adefc303
MahAmorim <marcela.amorim@muralis.com.br>
2025-09-05 19:41:39 -0300

    Adiciona next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 // next.config.js
-const isProd = process.env.NODE_ENV === "production";
-const repo = "bugbuster-culture"; // <- nome exato do repositório
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: "export",               // gera site estático em ./out
-  trailingSlash: true,            // GH Pages curte /about/ -> about/index.html
-  images: { unoptimized: true },  // next/image sem loader no export
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
+const nextConfig = {
+  output: "export",
+  basePath: "/bugbuster-culture",
+  assetPrefix: "/bugbuster-culture/",
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
+module.exports = nextConfig;
